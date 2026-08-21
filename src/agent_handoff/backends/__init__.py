434d30bdf0534f8da62df7b0a567cc0da505b224
minghao zhong <62844310+knightzhong@users.noreply.github.com>
2026-08-21 11:45:30 +0800

    feat: add backend exports

---
+++ src/agent_handoff/backends/__init__.py
@@ -0,0 +1,9 @@
+from .command import BrowserBridgeBackend, CommandBackend
+from .http import OllamaBackend, OpenAICompatibleBackend
+
+__all__ = [
+    "BrowserBridgeBackend",
+    "CommandBackend",
+    "OllamaBackend",
+    "OpenAICompatibleBackend",
+]
